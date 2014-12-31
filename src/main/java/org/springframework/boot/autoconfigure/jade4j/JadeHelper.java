c3e1ff6400bc080efcc85c007d125055a5844d21
Domingo Suarez Torres <domingo.suarez@gmail.com>
2014-12-31 01:50:57 -0600

    fixes javadoc

--- src/main/java/org/springframework/boot/autoconfigure/jade4j/JadeHelper.java
+++ src/main/java/org/springframework/boot/autoconfigure/jade4j/JadeHelper.java
@@ -22,15 +22,12 @@ import java.lang.annotation.*;
 
 /**
  * Indicates that an annotated class is a "Jade Helper" (e.g. utility object in the jade context).
- *
+ * <p/>
  * <p>This annotation serves as a specialization of {@link Component @Component},
  * allowing for implementation classes to be autodetected through classpath scanning.
- * It is typically used in combination with annotated handler methods based on the
- * {@link org.springframework.web.bind.annotation.RequestMapping} annotation.
  *
  * @author Domingo Suarez Torres
  * @see org.springframework.stereotype.Component
- * @see org.springframework.web.bind.annotation.RequestMapping
  * @see org.springframework.context.annotation.ClassPathBeanDefinitionScanner
  */
 @Target(ElementType.TYPE)
@@ -41,6 +38,7 @@ public @interface JadeHelper {
   /**
    * The value may indicate a suggestion for a logical component name,
    * to be turned into a Spring bean in case of an autodetected component.
+   *
    * @return the suggested component name, if any
    */
   String value() default "";
